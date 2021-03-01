4fb8bf078bb3ab1535855436e95a656d30e2d30b
Stephen Blythe <sblythe@lindenwood.edu>
2021-03-01 09:43:16 -0600

    Updated with initial rotation code, **CONTAINS KNOWN BUGS WRT HEIGHT**

--- AVLNode/AVLNode.cpp
+++ AVLNode/AVLNode.cpp
@@ -4,6 +4,69 @@
 
 using namespace std;
 
+AVLNode* rotateWithLeftChild(AVLNode *k2);
+AVLNode* rotateWithRightChild(AVLNode *k2);
+
+int getHeight(AVLNode *n)
+{
+  if (!n) // (n==NULL)
+    {
+      return 0;
+    }
+  else
+    {
+      return n->height();
+    }
+}
+
+std::ostream&
+AVLNode::print(std::ostream &os) const
+{
+  os << _val
+     << " (" << getHeight(_left) << "<->" << getHeight(_right) << ")";
+  return os;
+}
+
+
+AVLNode* rotateWithLeftChild(AVLNode *k2)
+{
+  AVLNode *k1 = k2->left();
+  k2->left() = k1->right(); // Y
+  k1->right() =  k2;
+
+  k2->height() = 1 + max (k2->left()->height(), k2->right()->height() );
+  k1->height() = 1 + max (k1->left()->height(), k2->height() );
+
+  return k1;
+}
+
+AVLNode* doubleWithLeftChild(AVLNode *k3)
+{
+  AVLNode *k1 = k3->left();
+  k3->left() = rotateWithRightChild(k1);
+  return rotateWithLeftChild(k3);
+}
+
+AVLNode* rotateWithRightChild(AVLNode *k2)
+{
+  AVLNode *k1 = k2->right();
+  k2->right() = k1->left(); // Y
+  k1->left() =  k2;
+
+  k2->height() = 1 + max (k2->left()->height(), k2->right()->height() );
+  k1->height() = 1 + max (k1->left()->height(), k2->height() );
+
+  return k1;
+}
+
+AVLNode* doubleWithRightChild(AVLNode *k3)
+{
+  AVLNode *k1 = k3->right();
+  k3->right() = rotateWithLeftChild(k1);
+  return rotateWithRightChild(k3);
+}
+
+
 
 AVLNode*
 AVLNode::insert(int newVal, AVLNode *intoSubTree)
@@ -19,20 +82,57 @@ AVLNode::insert(int newVal, AVLNode *intoSubTree)
       intoSubTree->_left=insert(newVal, intoSubTree->_left);
 
       // update _height for AVL manipulations
+      int leftHeight = getHeight(_left);
+
+      int rightHeight = getHeight(_right);
 
       // might need AVL rotations
+      if (leftHeight-rightHeight==2) // imbalance in left
+	{
+	  // is this a left-left imbalance?
+	  if (newVal < intoSubTree->_left->_val )
+	    {
+	      intoSubTree=rotateWithLeftChild(intoSubTree);
+	    }
+	  // or is it a left-rght imabalance?
+	  else
+	    {
+	      intoSubTree=doubleWithLeftChild(intoSubTree);
+	    }
+	}
     }
   else if (newVal > intoSubTree->_val)
     {
       intoSubTree->_right=insert(newVal, intoSubTree->_right);
 
       // update _height for AVL manipulations
+      int leftHeight = getHeight(_left);
+
+      int rightHeight = getHeight(_right);
 
       // might need AVL rotations
+      if (rightHeight-leftHeight==2) // imbalance in right
+	{
+	  // is this a right-right imbalance?
+	  if (newVal >  intoSubTree->_right->_val )
+	    {
+	      intoSubTree=rotateWithRightChild(intoSubTree);
+	    }
+	  // or is it a right-left imabalance?
+	  else
+	    {
+	      intoSubTree=doubleWithRightChild(intoSubTree);
+	    }
+	}
+
     }
   else // node value already in tree (==)
     {
     }
 
+  intoSubTree->height() = 1 + max(getHeight(intoSubTree->left()),
+				  getHeight(intoSubTree->right()) ); 
+  
+  
   return intoSubTree;
 }
--- AVLNode/AVLNode.hpp
+++ AVLNode/AVLNode.hpp
@@ -13,13 +13,31 @@ private:
 public:
   AVLNode(int val):_val(val), _left(nullptr), _right(nullptr), _height(1) {}
 
-  AVLNode* left() const {return _left;}
-  AVLNode* right() const {return _right;}
+  int value() const {return _val;}
+  int& value() {return _val;}
   
-  std::ostream& print(std::ostream &os) const {os << _val; return os;}
+  AVLNode* left() const {return _left;}
+  AVLNode* &left() {return _left;}
 
-  AVLNode* insert(int newVal, AVLNode *intoSubTree); 
+  AVLNode* right() const {return _right;}
+  AVLNode* &right() {return _right;}
 
+  int height() const {return _height;}
+  int &height()  {return _height;}
+
+  bool isLeaf() const {return !_left && !_right;}
+  
+  std::ostream& print(std::ostream &os) const;
+  /*{
+    os << _val << " (" << _left->_height << "<->" << _right->_height << ")";
+    return os;
+  }
+  */
+  
+  AVLNode* insert(int newVal, AVLNode *intoSubTree);
+  AVLNode* remove(int exstingVal, AVLNode *fromSubTree); 
+
+  friend  std::ostream& operator<<(std::ostream &os, const AVLNode &n); 
 };
 
 #endif
